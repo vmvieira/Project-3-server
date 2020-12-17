bd1ab4c68ac79e3639616ede294215d95b0d3018
Vitor Machado Vieira <vmvieira55@gmail.com>
2020-12-17 20:41:09 -0300

    testing deploy

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ require("./config/db.config")();
 const app = express();
 
 app.use(express.json());
-// Não esquecer de criar variável de ambiente com o endereço do seu app React (local ou deployado no Netlify)
-//app.use(cors());
+
 app.use(cors({ origin: process.env.REACT_APP_URL }));
 require("./config/passport.config")(app);
 
